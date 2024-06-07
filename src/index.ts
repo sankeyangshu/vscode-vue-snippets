61355653bc071ba43d7b3abbccce1db1f5708bb1
sankeyangshu <1260323835@qq.com>
2024-06-07 09:28:56 +0800

    fix: code snippet not generating

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { commands, ConfigurationChangeEvent, ExtensionContext, window, workspace } from 'vscode';
+import { commands, ConfigurationChangeEvent, window, workspace } from 'vscode';
 import { EXTENSION_NAME } from './config';
 import { generateSnippets } from './utils/gernerateSnippets';
 
@@ -7,9 +7,6 @@ import { generateSnippets } from './utils/gernerateSnippets';
  */
 const showRestartExtensionMessage = async ({ affectsConfiguration }: ConfigurationChangeEvent) => {
   if (affectsConfiguration(EXTENSION_NAME)) {
-    // 生成代码片段
-    await generateSnippets();
-
     // 提示用户重启扩展
     setTimeout(() => {
       window
@@ -27,24 +24,15 @@ const showRestartExtensionMessage = async ({ affectsConfiguration }: Configurati
   }
 };
 
-export async function activate(context: ExtensionContext) {
-  // 获取插件的全局状态
-  const globalState = context.globalState;
-
-  // 检查代码片段是否已经生成
-  const hasGeneratedSnippets = globalState.get<boolean>('hasGeneratedVueSnippets');
-
-  // 如果代码片段还没有生成
-  if (!hasGeneratedSnippets) {
-    // 生成代码片段
-    await generateSnippets();
-
-    // 设置标志，表示代码片段已经生成
-    globalState.update('hasGeneratedVueSnippets', true);
-  }
-
+/**
+ * 激活扩展
+ */
+export async function activate() {
   // 设置配置变化的监听器
   workspace.onDidChangeConfiguration(showRestartExtensionMessage);
+
+  // 生成代码片段
+  await generateSnippets();
 }
 
 export function deactivate() {}
